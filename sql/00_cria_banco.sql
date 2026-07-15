e8be576cf7a209f371b42a3b8678645784b003f0
Gabriel-Jonsson <mucacas@gmail.com>
2026-07-15 15:56:03 -0300

    scripts de criacao do banco e tabelas

---
+++ sql/00_cria_banco.sql
@@ -0,0 +1,5 @@
+/* Cria o banco. Ajuste o caminho conforme a maquina. Rodar com isql SEM estar conectado a outro banco. */
+
+CREATE DATABASE 'localhost:C:\dados\granja.fdb'
+    USER 'SYSDBA' PASSWORD 'masterkey'
+    DEFAULT CHARACTER SET UTF8;
