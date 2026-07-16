79312309dc50a44591bf62d1b4f26e146de81af8
Gabriel-Jonsson <mucacas@gmail.com>
2026-07-16 10:01:07 -0300

    tela principal com grid de lotes e indicador de saude

--- src/ufrmPrincipal.dfm
+++ src/ufrmPrincipal.dfm
@@ -1,8 +1,9 @@
-object Form1: TForm1
+object frmPrincipal: TfrmPrincipal
   Left = 0
   Top = 0
-  ClientHeight = 541
-  ClientWidth = 1062
+  Caption = 'Controle de Lotes de Aves'
+  ClientHeight = 460
+  ClientWidth = 860
   Color = clBtnFace
   Font.Charset = DEFAULT_CHARSET
   Font.Color = clWindowText
@@ -10,6 +11,117 @@ object Form1: TForm1
   Font.Name = 'Tahoma'
   Font.Style = []
   OldCreateOrder = False
+  Position = poScreenCenter
+  OnCreate = FormCreate
+  OnDestroy = FormDestroy
   PixelsPerInch = 96
   TextHeight = 13
+  object pnlSaude: TPanel
+    Left = 0
+    Top = 0
+    Width = 860
+    Height = 48
+    Align = alTop
+    BevelOuter = bvNone
+    Caption = 'Nenhum lote selecionado'
+    Font.Charset = DEFAULT_CHARSET
+    Font.Color = clWindowText
+    Font.Height = -13
+    Font.Name = 'Tahoma'
+    Font.Style = [fsBold]
+    ParentBackground = False
+    ParentFont = False
+    TabOrder = 0
+  end
+  object grdLotes: TDBGrid
+    Left = 0
+    Top = 48
+    Width = 860
+    Height = 371
+    Align = alClient
+    DataSource = dsLotes
+    Options = [dgTitles, dgIndicator, dgColumnResize, dgColLines, dgRowLines, dgTabs, dgRowSelect, dgCancelOnExit, dgTitleClick, dgTitleHotTrack]
+    ReadOnly = True
+    TabOrder = 1
+    TitleFont.Charset = DEFAULT_CHARSET
+    TitleFont.Color = clWindowText
+    TitleFont.Height = -11
+    TitleFont.Name = 'Tahoma'
+    TitleFont.Style = []
+    OnDrawColumnCell = grdLotesDrawColumnCell
+    Columns = <
+      item
+        Expanded = False
+        FieldName = 'O_ID_LOTE'
+        Title.Caption = 'C'#243'digo'
+        Width = 55
+        Visible = True
+      end
+      item
+        Expanded = False
+        FieldName = 'O_DESCRICAO'
+        Title.Caption = 'Descri'#231#227'o'
+        Width = 280
+        Visible = True
+      end
+      item
+        Expanded = False
+        FieldName = 'O_DATA_ENTRADA'
+        Title.Caption = 'Entrada'
+        Width = 80
+        Visible = True
+      end
+      item
+        Expanded = False
+        FieldName = 'O_QUANTIDADE_INICIAL'
+        Title.Caption = 'Qtde inicial'
+        Width = 80
+        Visible = True
+      end
+      item
+        Expanded = False
+        FieldName = 'O_PESO_MEDIO_GERAL'
+        Title.Caption = 'Peso m'#233'dio geral'
+        Width = 100
+        Visible = True
+      end
+      item
+        Expanded = False
+        FieldName = 'O_MORTALIDADE_ACUMULADA'
+        Title.Caption = 'Mortas'
+        Width = 60
+        Visible = True
+      end
+      item
+        Expanded = False
+        FieldName = 'O_PERCENTUAL_MORTALIDADE'
+        Title.Caption = 'Mortalidade %'
+        Width = 100
+        Visible = True
+      end>
+  end
+  object pnlRodape: TPanel
+    Left = 0
+    Top = 419
+    Width = 860
+    Height = 41
+    Align = alBottom
+    BevelOuter = bvNone
+    TabOrder = 2
+    object btnAtualizar: TButton
+      Left = 8
+      Top = 8
+      Width = 90
+      Height = 25
+      Caption = 'Atualizar'
+      TabOrder = 0
+      OnClick = btnAtualizarClick
+    end
+  end
+  object dsLotes: TDataSource
+    DataSet = dmPrincipal.qryLotes
+    OnDataChange = dsLotesDataChange
+    Left = 792
+    Top = 104
+  end
 end
--- src/ufrmPrincipal.pas
+++ src/ufrmPrincipal.pas
@@ -4,21 +4,128 @@ interface
 
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs;
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.Grids, Vcl.DBGrids, Vcl.StdCtrls,
+  Vcl.ExtCtrls, Data.DB,
+  uEntidades;
 
 type
-  TForm1 = class(TForm)
+  TfrmPrincipal = class(TForm)
+    pnlSaude: TPanel;
+    grdLotes: TDBGrid;
+    pnlRodape: TPanel;
+    btnAtualizar: TButton;
+    dsLotes: TDataSource;
+    procedure FormCreate(Sender: TObject);
+    procedure FormDestroy(Sender: TObject);
+    procedure dsLotesDataChange(Sender: TObject; Field: TField);
+    procedure btnAtualizarClick(Sender: TObject);
+    procedure grdLotesDrawColumnCell(Sender: TObject; const Rect: TRect;
+      DataCol: Integer; Column: TColumn; State: TGridDrawState);
   private
-    { Private declarations }
-  public
-    { Public declarations }
+    FLote: TLoteAves;
+    procedure CarregaLoteSelecionado;
+    procedure AtualizaIndicador;
+    function CorDaFaixa(AFaixa: TFaixaSaude): TColor;
   end;
 
 var
-  Form1: TForm1;
+  frmPrincipal: TfrmPrincipal;
 
 implementation
 
 {$R *.dfm}
+
+uses
+  udmPrincipal;
+
+procedure TfrmPrincipal.FormCreate(Sender: TObject);
+begin
+  FLote := TLoteAves.Create;
+  dmPrincipal.AbreLotes;
+end;
+
+procedure TfrmPrincipal.FormDestroy(Sender: TObject);
+begin
+  FLote.Free;
+end;
+
+procedure TfrmPrincipal.dsLotesDataChange(Sender: TObject; Field: TField);
+begin
+  { dispara a cada scroll do grid - mantem o lote selecionado e o
+    indicador sempre em dia }
+  CarregaLoteSelecionado;
+  AtualizaIndicador;
+end;
+
+procedure TfrmPrincipal.CarregaLoteSelecionado;
+begin
+  if dmPrincipal.qryLotes.IsEmpty then
+  begin
+    FLote.Id := 0;
+    Exit;
+  end;
+
+  FLote.Id := dmPrincipal.qryLotes.FieldByName('O_ID_LOTE').AsInteger;
+  FLote.Descricao := dmPrincipal.qryLotes.FieldByName('O_DESCRICAO').AsString;
+  FLote.DataEntrada := dmPrincipal.qryLotes.FieldByName('O_DATA_ENTRADA').AsDateTime;
+  FLote.QuantidadeInicial := dmPrincipal.qryLotes.FieldByName('O_QUANTIDADE_INICIAL').AsInteger;
+  FLote.PesoMedioGeral := dmPrincipal.qryLotes.FieldByName('O_PESO_MEDIO_GERAL').AsFloat;
+  FLote.AtualizaMortalidade(
+    dmPrincipal.qryLotes.FieldByName('O_MORTALIDADE_ACUMULADA').AsInteger,
+    dmPrincipal.qryLotes.FieldByName('O_PERCENTUAL_MORTALIDADE').AsFloat);
+end;
+
+procedure TfrmPrincipal.AtualizaIndicador;
+begin
+  if FLote.Id = 0 then
+  begin
+    pnlSaude.Color := clBtnFace;
+    pnlSaude.Font.Color := clWindowText;
+    pnlSaude.Caption := 'Nenhum lote selecionado';
+    Exit;
+  end;
+
+  pnlSaude.Color := CorDaFaixa(FLote.FaixaSaude);
+  if FLote.FaixaSaude = fsAmarelo then
+    pnlSaude.Font.Color := clBlack
+  else
+    pnlSaude.Font.Color := clWhite;
+  pnlSaude.Caption := Format('%s  -  mortalidade acumulada: %d de %d aves (%.2f%%)',
+    [FLote.Descricao, FLote.MortalidadeAcumulada, FLote.QuantidadeInicial,
+     FLote.PercentualMortalidade]);
+end;
+
+function TfrmPrincipal.CorDaFaixa(AFaixa: TFaixaSaude): TColor;
+begin
+  case AFaixa of
+    fsVerde:    Result := TColor($0050AF4C);
+    fsAmarelo:  Result := TColor($0007C1FF);
+  else
+    Result := TColor($003643F4);
+  end;
+end;
+
+procedure TfrmPrincipal.grdLotesDrawColumnCell(Sender: TObject; const Rect: TRect;
+  DataCol: Integer; Column: TColumn; State: TGridDrawState);
+begin
+  { pinta a celula do percentual com a cor da faixa - a classificacao vem
+    da mesma funcao usada pelo indicador, nada duplicado }
+  if SameText(Column.FieldName, 'O_PERCENTUAL_MORTALIDADE') and
+     not (gdSelected in State) then
+  begin
+    grdLotes.Canvas.Brush.Color :=
+      CorDaFaixa(TLoteAves.ClassificaSaude(Column.Field.AsFloat));
+    if TLoteAves.ClassificaSaude(Column.Field.AsFloat) = fsAmarelo then
+      grdLotes.Canvas.Font.Color := clBlack
+    else
+      grdLotes.Canvas.Font.Color := clWhite;
+  end;
+  grdLotes.DefaultDrawColumnCell(Rect, DataCol, Column, State);
+end;
+
+procedure TfrmPrincipal.btnAtualizarClick(Sender: TObject);
+begin
+  dmPrincipal.AbreLotes;
+end;
 
 end.
